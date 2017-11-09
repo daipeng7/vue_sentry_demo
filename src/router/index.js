0762ee2f2cd12f5b561d9415fa916c939251d4c0
daipeng <daipeng@doctorwork.com>
2017-11-09 18:25:01 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/pages/hello'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: HelloWorld
+    }
+  ]
+})
